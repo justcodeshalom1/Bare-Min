82d4d752142ccac5d79364a2ae391465a29990b0
Salvador Desravines <desravines.salvador001@yahoo.com>
2020-04-23 15:08:04 +0000

    added setstore to flux.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -19,7 +19,7 @@ const getState = ({ getStore, setStore }) => {
 					.then(resp => resp.json())
 					.then(data => {
 						const store = getStore();
-						setStore({ store: product });
+						data => setStore({ store: product });
 						console.log(data);
 					});
 			}
